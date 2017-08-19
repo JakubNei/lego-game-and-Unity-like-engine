dcb26f3737b32a86c935ea999d4f770dab7a457f
aeroson <theaeroson@gmail.com>
2017-08-19 09:45:49 +0200

    improved resources folder determination

--- MyEngine/myengine/Resource.cs
+++ MyEngine/myengine/Resource.cs
@@ -6,79 +6,82 @@ using System.Text;
 
 namespace MyEngine
 {
-    public class Resource
-    {
-        public string originalPath;
-        public string realPath;
+	public class Resource
+	{
+		public string originalPath;
+		public string realPath;
 
-        private Resource()
-        {
+		private Resource()
+		{
 
-        }
-               
-        public static implicit operator string(Resource r)
-        {
-            return r.realPath;
-        }
-        public static implicit operator Resource(string originalPath)
-        {
-            return MakeResource(originalPath);
-        }
+		}
 
-        public static Resource WithAllPathsAs(string path)
-        {
-            var r=new Resource();
-            r.originalPath = UseCorrectDirectorySeparator(path);
-            r.realPath = UseCorrectDirectorySeparator(path);
-            return r;
-        }
+		public static implicit operator string(Resource r)
+		{
+			return r.realPath;
+		}
+		public static implicit operator Resource(string originalPath)
+		{
+			return MakeResource(originalPath);
+		}
 
-        public static string UseCorrectDirectorySeparator(string path)
-        {
-            path = path.Replace('/', System.IO.Path.DirectorySeparatorChar);
-            path = path.Replace('\\', System.IO.Path.DirectorySeparatorChar);
-            return path;
-        }
+		public static Resource WithAllPathsAs(string path)
+		{
+			var r = new Resource();
+			r.originalPath = UseCorrectDirectorySeparator(path);
+			r.realPath = UseCorrectDirectorySeparator(path);
+			return r;
+		}
 
-        public static bool ResourceInFolderExists(Resource folder, string childName)
-        {
-            var lastSlash = folder.originalPath.LastIndexOf("/");
-            if (lastSlash == -1) lastSlash = 0;
-            var originalPath = folder.originalPath.Substring(0, lastSlash) + childName;
+		public static string UseCorrectDirectorySeparator(string path)
+		{
+			path = path.Replace('/', System.IO.Path.DirectorySeparatorChar);
+			path = path.Replace('\\', System.IO.Path.DirectorySeparatorChar);
+			return path;
+		}
 
-            return File.Exists(MakeRealPath(originalPath));
-        }
+		public static bool ResourceInFolderExists(Resource folder, string childName)
+		{
+			var lastSlash = folder.originalPath.LastIndexOf("/");
+			if (lastSlash == -1) lastSlash = 0;
+			var originalPath = folder.originalPath.Substring(0, lastSlash) + childName;
+
+			return File.Exists(MakeRealPath(originalPath));
+		}
 
 
 
-        public static Resource GetResourceInFolder(Resource folder, string childName)
-        {            
-            var lastSlash = folder.originalPath.LastIndexOf("/");
-            if (lastSlash == -1) lastSlash = 0;
-            var originalPath = folder.originalPath.Substring(0, lastSlash) + childName;
+		public static Resource GetResourceInFolder(Resource folder, string childName)
+		{
+			var lastSlash = folder.originalPath.LastIndexOf("/");
+			if (lastSlash == -1) lastSlash = 0;
+			var originalPath = folder.originalPath.Substring(0, lastSlash) + childName;
 
-            return MakeResource(originalPath);
-        }
+			return MakeResource(originalPath);
+		}
 
-        private static string MakeRealPath(string originalPath)
-        {
-            return UseCorrectDirectorySeparator("../../../Resources/" + originalPath);
-        }
-        
-        private static Resource MakeResource(string originalPath)
-        {
-            var realPath = MakeRealPath(originalPath);
-            if (File.Exists(realPath))
-            {
-                return new Resource() { originalPath = originalPath, realPath = realPath };
-            }
-            else
-            {
-                Debug.Error("File "+originalPath + " doesnt exits");
-                Debug.Pause();
-                return null;
-            }
-        }
 
-    }
+
+
+		private static string MakeRealPath(string originalPath)
+		{
+			return UseCorrectDirectorySeparator(ResourceFolder.GetDefaultResourcesFolder() + originalPath);
+		}
+
+		private static Resource MakeResource(string originalPath)
+		{
+			var realPath = MakeRealPath(originalPath);
+			if (File.Exists(realPath))
+			{
+				return new Resource() { originalPath = originalPath, realPath = realPath };
+			}
+			else
+			{
+				Debug.Error("File " + originalPath + " doesnt exits");
+				Debug.Pause();
+				return null;
+			}
+		}
+
+	}
 }
--- MyEngine/myengine/ResourceFolder.cs
+++ MyEngine/myengine/ResourceFolder.cs
@@ -6,43 +6,67 @@ using System.Text;
 
 namespace MyEngine
 {
-    public class ResourceFolder
-    {
-        public string originalPath;
-        public string realPath;
+	public class ResourceFolder
+	{
+		public string originalPath;
+		public string realPath;
 
-        private ResourceFolder()
-        {
+		private ResourceFolder()
+		{
 
-        }
+		}
 
-        public static implicit operator string (ResourceFolder r)
-        {
-            return r.realPath;
-        }
-        public static implicit operator ResourceFolder(string originalPath)
-        {
-            return MakeResource(originalPath);
-        }
-        private static string MakeRealPath(string originalPath)
-        {
-            return "../../../Resources/" + originalPath;
-        }
+		public static implicit operator string(ResourceFolder r)
+		{
+			return r.realPath;
+		}
+		public static implicit operator ResourceFolder(string originalPath)
+		{
+			return MakeResource(originalPath);
+		}
 
-        private static ResourceFolder MakeResource(string originalPath)
-        {
-            var realPath = MakeRealPath(originalPath);
-            if (Directory.Exists(realPath))
-            {
-                return new ResourceFolder() { originalPath = originalPath, realPath = realPath };
-            }
-            else
-            {
-                Debug.Error("Directory " + originalPath + " doesnt exits");
-                Debug.Pause();
-                return null;
-            }
-        }
+		static string defaultResourcesFolder;
+		public static string GetDefaultResourcesFolder()
+		{
+			if (defaultResourcesFolder != null)
+				return defaultResourcesFolder;
 
-    }
+			Debug.Info("root path: " + Path.GetFullPath("."));
+
+			defaultResourcesFolder = "resources/";
+			for (int i = 0; i < 10; i++)
+			{
+				if (Directory.Exists("./" + defaultResourcesFolder))
+				{
+					defaultResourcesFolder = "./" + defaultResourcesFolder;
+					return defaultResourcesFolder;
+				}
+
+				defaultResourcesFolder = "../" + defaultResourcesFolder;
+			}
+
+			return defaultResourcesFolder;
+		}
+
+		private static string MakeRealPath(string originalPath)
+		{
+			return GetDefaultResourcesFolder() + originalPath;
+		}
+
+		private static ResourceFolder MakeResource(string originalPath)
+		{
+			var realPath = MakeRealPath(originalPath);
+			if (Directory.Exists(realPath))
+			{
+				return new ResourceFolder() { originalPath = originalPath, realPath = realPath };
+			}
+			else
+			{
+				Debug.Error("Directory " + originalPath + " doesnt exits");
+				Debug.Pause();
+				return null;
+			}
+		}
+
+	}
 }
